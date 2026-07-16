f79256bfeccc4d4840a120c20ff3411fcb4a7068
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-07-16 22:03:14 +0530

    feat: add github issue collector

---
+++ collectors/github/repositories.py
@@ -0,0 +1,12 @@
+TARGET_REPOS = [
+    "microsoft/vscode",
+    "facebook/react",
+    "vercel/next.js",
+    "langchain-ai/langchain",
+    "huggingface/transformers",
+    "pytorch/pytorch",
+    "tensorflow/tensorflow",
+    "fastapi/fastapi",
+    "docker/compose",
+    "kubernetes/kubernetes",
+]
