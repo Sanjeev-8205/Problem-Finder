efdbcac94d1c3aa0a3cf97a05aa37eb3a472bdcf
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-07-17 21:10:28 +0530

    fear: add select

---
+++ frontend/src/components/routing/ScrollToHash.tsx
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+function ScrollToHash() {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (!hash) return;
+
+    const timeoutId = setTimeout(() => {
+      const element = document.querySelector(hash);
+
+      if (element) {
+        element.scrollIntoView({
+          behavior: "smooth",
+          block: "start",
+        });
+      }
+    }, 100);
+
+    return () => clearTimeout(timeoutId);
+  }, [hash, pathname]);
+
+  return null;
+}
+
+export default ScrollToHash;
