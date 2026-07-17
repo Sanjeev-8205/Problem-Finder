a5b0848abbf9a59c58e009fb11df55f5f79e8eec
Sanjeev-8205 <sanjeev.820525@gmail.com>
2026-07-17 21:15:07 +0530

    feat: add motion div for problem cards

--- frontend/src/components/cards/ClusterGrid.tsx
+++ frontend/src/components/cards/ClusterGrid.tsx
@@ -1,5 +1,6 @@
 import ClusterCard from "@/components/cards/ClusterCard";
 import type { Problem } from "@/types/problem";
+import { motion } from "framer-motion";
 
 type ClusterGridProps = {
   problems: Problem[];
@@ -12,13 +13,23 @@ function ClusterGrid({
 }: ClusterGridProps) {
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-2 xl:grid-cols-3">
-      {problems.map((problem) => (
+      {problems.map((problem, index) => (
+        <motion.div
+        key={problem.cluster_id}
+        initial={{ opacity: 0, y: 16 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true, amount: 0.15 }}
+        transition={{
+            duration: 0.3,
+            delay: index * 0.01,
+        }}
+        >
         <ClusterCard
-          key={problem.cluster_id}
-          cluster={problem}
-          onClick={() => onSelectCluster(problem)}
+            cluster={problem}
+            onClick={() => onSelectCluster(problem)}
         />
-      ))}
+        </motion.div>
+        ))}
     </div>
   );
 }
